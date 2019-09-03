0c48f42f531de0d330ab8075456a8d4ae9d56150
myurch <36048692+myurch@users.noreply.github.com>
2019-09-02 18:55:05 -0700

    stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import {DataBase} from './database'
 import {createField} from './utils'
+import {BASIC, OBJECT, BACKREF} from './utils'
 
 export {DataBase}
 export {createField}
+export {BASIC, OBJECT, BACKREF}
